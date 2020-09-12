01e07edc01169e23e6c33a5857e7b3792245740e
reginaldo melo <reginaldo.melo56@gmail.com>
2020-09-11 23:43:49 -0300

    Updates

---
+++ precache-manifest.84f4803a59a63bd9361fbb6aa1d17e78.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e089bb0471c88806b69dbb4cff211657",
+    "url": "/csschallenges/index.html"
+  },
+  {
+    "revision": "bc1ca313193429b5eaf1",
+    "url": "/csschallenges/static/css/main.0115ab04.chunk.css"
+  },
+  {
+    "revision": "a587ec9c3fbda54cb4d7",
+    "url": "/csschallenges/static/js/2.2d313609.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/csschallenges/static/js/2.2d313609.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc1ca313193429b5eaf1",
+    "url": "/csschallenges/static/js/main.cd8ba77c.chunk.js"
+  },
+  {
+    "revision": "0c14feac1cb2ab15eff8",
+    "url": "/csschallenges/static/js/runtime-main.aca147b9.js"
+  },
+  {
+    "revision": "f9a6c0dbcbba58636e4f926fbb9da757",
+    "url": "/csschallenges/static/media/bota.f9a6c0db.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/csschallenges/static/media/logo.5d5d9eef.svg"
+  }
+]);
